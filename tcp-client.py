6be7a3c3570e3f631e66d386c80253f9e56d62fd
Rodrigo F Martins <martins.drg@gmail.com>
2017-04-16 13:25:16 -0300

    Usar instrução with para finalizar socket

--- tcp-client.py
+++ tcp-client.py
@@ -4,20 +4,14 @@
 
 import socket
 
-target_host = "google.com"
+target_host = "duckduckgo.com"
 target_port = 80
 
 # Criar um objeto de socket
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Conectar o cliente
-client.connect((target_host, target_port))
-
-# Enviar alguns dados
-request = "GET / HTTP/1.1\r\nHost: " + target_host + "\r\n\r\n"
-client.send(request.encode('utf-8'))
-
-# Receber alguns dados
-response = client.recv(4096)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((target_host, target_port))
+    request = "GET / HTTP/1.1\r\nHost: " + target_host + "\r\n\r\n"
+    s.sendall(request.encode('utf-8'))
+    response = s.recv(4096)
 
 print(response)
